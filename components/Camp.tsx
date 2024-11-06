e0d9f1321e7d83968ecb8dd53a2d21f8fef66782
GMwanda <george.mwanda@strathmore.edu>
2024-11-06 13:56:37 +0300

    completed travel website

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,7 +1,68 @@
+import Image from "next/image";
+import {PEOPLE_URL} from "@/constants";
+
+interface CampProps {
+    backgroundImage: string;
+    title: string;
+    subtitle: string;
+    peopleJoined: string;
+}
+
+const CampSite = ({backgroundImage, title, subtitle, peopleJoined}: CampProps) => {
+    return (
+        <div
+            className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat rounded-5xl`}>
+            <div className='flex h-full flex-col items-start justify-between p-6 lg:py-10 lg:px-20'>
+                <div className='flexCenter gap-4'>
+                    <div className='rounded-full bg-green-50 p-4'>
+                        <Image src='/folded-map.svg' alt='map' width={28} height={28}/>
+                    </div>
+                    <div className='flex flex-col gap-1'>
+                        <h4 className='bold-18 text-white'>{title}</h4>
+                        <p className='regular-14 text-white'>{subtitle}</p>
+                    </div>
+                </div>
+                <div className='flexCenter gap-6'>
+                    <span className='flex -space-x-4 overflow-hidden'>
+                        {PEOPLE_URL.map((url) => (
+                            <Image src={url} alt='person' width={52} height={52}
+                                   className='inline-block h-10 w-10 rounded-full'/>
+                        ))}
+                    </span>
+                    <p className='bold-16 md:bold-20 text-white capitalize'>{peopleJoined}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 const Camp = () => {
     return (
-        <div>Camp</div>
+        <section
+            className='2xl:max-container relative flex flex-col px-10 lg:mb-10 lg:py-20 xl:mb-20'>
+            <div
+                className='hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]'>
+                <CampSite backgroundImage='bg-bg-img-1' title='Tokyo, Japan' subtitle='Hurghada, Egypt'
+                          peopleJoined='10+ joined'
+                />
+                <CampSite backgroundImage='bg-bg-img-2' title='Tokyo, Japan' subtitle='somewhere in the wild'
+                          peopleJoined='45+ joined'/>
+            </div>
+            <div className='flexEnd mt-10 px-6 lg:-mt-40 lg:mr-6'>
+                <div
+                    className='bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative overflow-hidden w-full rounded-3xl'>
+                    <h2 className='regular-24 md:regular-32 2xl:regular-64 capitalize text-white'><strong>Feeling
+                        lost</strong> And not sure about the way?
+                    </h2>
+                    <p className='regular-14 xl:regular-16 mt-5 text-white'>Travellers on the hunt for something a
+                        little different from their next European adventure are increasingly looking towards Albania.
+                        This piece of the Balkan jigsaw is drawing a rising number of visitors thanks to its Adriatic
+                        and Ionian coasts, intriguing Ottoman and Communist past, and relatively nascent tourism
+                        scene.</p>
+                    <Image src='/quote.svg' alt='quote' width={186} height={219} className='camp-quote'/>
+                </div>
+            </div>
+        </section>
     )
 }
 export default Camp
--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,7 +1,45 @@
+import Image from "next/image";
 
 const Guide = () => {
     return (
-        <div>Guide</div>
+        <section className='flexCenter flex-col'>
+            <div className='padding-container max-container w-full pb-24'>
+                <Image src='/camp.svg' alt='camp' width={50} height={50}/>
+                <p className='uppercase regular-18 -mt-1 mb-3 text-green-50'>we are here for you</p>
+                <div className='flex flex-wrap justify-between gap-5'>
+                    <h2 className='bold-40 lg:bold64 xl:max-w-[390px] capitalize'>guide you to easy path</h2>
+                    <p className='regular-16 text-gray-30 xl:max-w-[520px]'>Tucked into the northern interior, the
+                        Albanian Alps are equally part of the draw. Known, rather
+                        ominously, as the Accursed Mountains, this realm of jagged karst peaks is a dream for hikers,
+                        where lynxes roam spruce forests and wildflowers cover valley floors. Those looking to tackle
+                        the region’s challenging trails can set off on a new, eight-day, self-guided tour with Walks
+                        Worldwide, which offers an itinerary that includes treks through mountain passes, traditional
+                        homestays and a visit to the village of Theth, starting point of the Peaks of the Balkans hiking
+                        trail. </p>
+                </div>
+            </div>
+            <div className='flexCenter max-container relative w-full'>
+                <Image src='/boat.png' alt='boat' width={1440} height={580}
+                       className='w-full object-center object-cover 2xl:rounded-5xl'/>
+                <div
+                    className='absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20'>
+                    <Image src='/meter.svg' alt='meter' width={16} height={158} className='h-full w-auto'/>
+                    <div className='flexBetween flex-col'>
+                        <div className='flex w-full flex-col'>
+                            <div className='flexBetween w-full'>
+                                <p className='regular-16 text-gray-20 capitalize'>destination</p>
+                                <p className='bold-16 text-green-50'>30 min</p>
+                            </div>
+                            <p className='bold-20 mt-2'>Belfast, Northern Ireland</p>
+                        </div>
+                        <div className='flex w-full flex-col'>
+                            <p className='regular-16 text-gray-20 capitalize'>start track</p>
+                            <h4 className='bold-20 mt-2 whitespace-nowrap'>Emilia-Romagna, Italy</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 export default Guide
